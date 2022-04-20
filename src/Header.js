41f745785c611da5241e3082b9cd251a542b96b9
= <=>
2022-04-20 17:44:37 -0600

    Making the header compnent

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./Header.css";
+import PersonIcon from '@mui/icons-material/Person';
+import IconButton from '@mui/material/IconButton';
+import ForumIcon from '@mui/icons-material/Forum';
+import Tinder from "./tinder.png"
+
+function Header() {
+  return (
+    <div className='header'>
+
+      <IconButton>
+        <PersonIcon fontSize='large' className='header_icon' />    
+      </IconButton>
+
+      <img 
+        className='header_logo'
+        alt='Tinder'
+        src={Tinder}
+      />
+
+      <IconButton>
+        <ForumIcon fontSize='large' className='header_icon' />    
+      </IconButton>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./tinderClass.css";
+
+function TinderCards() {
+  return (
+    <div className='tinderClass'>
+      
+    </div>
+  )
+}
+
+export default TinderCards
