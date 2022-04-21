7cd65085ca7c41cf4cde0a3a3b85585fd1b6e6e8
= <=>
2022-04-20 18:10:11 -0600

    Initializing the tinder card

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TinderCard from 'react-tinder-card';
 import "./tinderClass.css";
 
 function TinderCards() {
+
+    const [people, setPeople] = useState([
+        {name: 'Elon Musk', url: ""},
+        {name: 'Luis Martínez', url: ""},
+        {name: 'Reyna Sorto', url: ""},
+        {name: 'Jheferson Pavón', url: ""},
+    ]);
+
+    // this one indicates when we swipe and don't want to see the person anymore
+    const swiped = (direction, nameToDelete) => {
+        console.log(`removing: ${nameToDelete}`);
+    }
+
+    const onOutFrame = (nameToDelete) => {
+        console.log(`${nameToDelete} left the screen`);
+    }
+
   return (
     <div className='tinderClass'>
-      
+        <div className="tinderCard_container">
+            {people.map((person) => (
+                <TinderCard
+                
+                className='swipe'
+                key={person.name}
+                preventSwipe={["up", "down"]}
+                onSwipe={(dir) => swiped(dir, person.name)}
+                onCardLeftScreen={() => onOutFrame(person.name)}
+                >
+
+                </TinderCard>
+            ))}
+        </div>
     </div>
   )
 }
